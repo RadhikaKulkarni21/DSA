222875422b8eb0dccf8d10a65b205457e43e298b
radhi <radhika2103@gmail.com>
2025-06-11 15:36:47 +0100

    Basic ops LL

---
+++ LinkedList/BasicLLOps.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int value;
+    Node* next;
+
+    Node(int value) {
+        this->value = value;
+        next = nullptr;
+    }
+};
+
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int length;
+
+public:
+    //Make a LL constructor
+    LinkedList(int value) {
+        Node* newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    //Make a destructor
+    ~LinkedList() {
+        Node* temp = head;
+        while (head) {
+            head = head->next;
+            delete temp;
+            temp = head;
+        }
+    }
+
+    //Print the linkedList
+    void printList() {
+        Node* temp = head;
+        if (temp == nullptr) {
+            cout << "empty";
+        }
+        else {
+            while (temp != nullptr) {
+                cout << temp->value;
+                temp = temp->next;
+                if (temp != nullptr) {
+                    cout << " -> ";
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    Node* getHead() {
+        return head;
+    }
+
+    Node* getTail() {
+        return tail;
+    }
+
+    int getLength() {
+        return length;
+    }
+
+    // We will use this method to test deleteNode on an empty list
+    void makeEmpty() {
+        Node* temp = head;
+        while (head) {
+            head = head->next;
+            delete temp;
+            temp = head;
+        }
+        tail = nullptr;
+        length = 0;
+    }
+
+    //adding new node at the end of LL
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    //Delete last node
+    void deleteLast() {
+        if (length == 0) return;
+        Node* temp = head;
+        if (length == 1) {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else {
+            Node* pre = head;
+            while (temp->next) {
+                pre = temp;
+                temp = temp->next;
+            }
+            tail = pre;
+            tail->next = nullptr;
+        }
+        delete temp;
+        length--;
+    }
+
+    //add node at the start of LL
+    void prepend(int value) {
+        Node* newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    //Delete the first node
+    void deleteFirst() {
+        if (length == 0) return;
+        Node* temp = head;
+        if (length == 1) {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else {
+            head = head->next;
+        }
+        delete temp;
+        length--;
+    }
+
+    //returns a pointer to the node at the specified index in the linked list.
+    Node* get(int index) {
+        if (index < 0 || index >= length) return nullptr;
+        Node* temp = head;
+        for (int i = 0; i < index; ++i) {
+            temp = temp->next;
+        }
+        return temp;
+    }
+
+    //function that updates the value of the node at the specified index in the linked list.
+    bool set(int index, int value) {
+        Node* temp = get(index);
+        if (temp) {
+            temp->value = value;
+            return true;
+        }
+        return false;
+    }
+
+    //Inserts a new node with the specified value at the specified index 
+    bool insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node* newNode = new Node(value);
+        Node* temp = get(index - 1);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        length++;
+        return true;
+    }
+
+    //deletes a new node with the specified value at the specified index 
+    void deleteNode(int index) {
+        if (index < 0 || index >= length) return;
+        if (index == 0) return deleteFirst();
+        if (index == index - 1) deleteFirst();
+
+        Node* prev = get(index - 1);
+        Node* temp = prev->next;
+        prev->next = temp->next;
+        delete temp;
+        length--;
+    }
+
+    //reverse a given LL
+    void reverse() {
+        Node* temp = head;
+        head = tail;
+        tail = temp;
+
+        Node* before = nullptr;
+        Node* after = temp->next;
+
+        for (int i = 0; i < length;i++) {
+            after = temp->next;
+            temp->next = before;
+            before = temp;
+            temp = after;
+        }
+    }
+};
