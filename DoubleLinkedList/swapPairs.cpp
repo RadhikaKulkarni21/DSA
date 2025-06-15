a8abbe92951ef6fd5598ba65f3e72f89cd126be7
radhi <radhika2103@gmail.com>
2025-06-15 17:42:47 +0100

    swapping two nodes

---
+++ DoubleLinkedList/swapPairs.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+
+using namespace std;
+
+class Node { 
+    public: 
+        int value;
+        Node* next;
+        Node* prev;
+    
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+    
+    public:
+        DoublyLinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+    
+        ~DoublyLinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+    
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty" << endl;
+                return;
+            }
+            while (temp->next != nullptr) {
+                cout << temp->value << " <-> ";
+                temp = temp->next;
+            }
+            cout << temp->value << endl;
+        }
+    
+        Node* getHead() {
+            return head;
+        }
+    
+        Node* getTail() {
+            return tail;
+        }
+    
+        int getLength() {
+            return length;
+        }
+
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (length == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+            length++;
+        }
+
+        //   +=====================================================+
+        //   |                 WRITE YOUR CODE HERE                |
+        //   | Description:                                        |
+        //   | - The swapPairs function swaps adjacent pairs       |
+        //   |   of nodes in a doubly linked list.                 |
+        //   | - Return type: void                                 |
+        //   |                                                     |
+        //   | Tips:                                               |
+        //   | - Utilizes a dummyNode to simplify edge cases.      |
+        //   | - Uses pointers to navigate and swap nodes.         |
+        //   | - Pay close attention to the 'next' and 'prev'      |
+        //   |   pointers of the nodes.                            |
+        //   | - Works in-place; doesn't create new nodes.         |
+        //   | - Always checks if the list is empty or has only    |
+        //   |   one node.                                         |
+        //   | - Check output from Test.cpp in "User logs".        |
+        //   +=====================================================+
+        void swapPairs(){
+            Node* dummy = new Node(0);
+            dummy->next = head;
+    
+            Node* previous = dummy;
+            Node* first = head;
+    
+            while (first != nullptr && first->next != nullptr) {
+                Node* second = first->next;
+    
+                previous->next = second;
+                first->next = second->next;
+                second->next = first;
+    
+                previous = first;
+                first = first->next;
+            }
+            head = dummy->next;
+            delete dummy;
+    }   
+
+};
+
+int main(){
+            cout << "\n----- Test: Swap Pairs with Multiple Nodes -----\n";
+        DoublyLinkedList list(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        cout << "DLL before swapping pairs:\n";
+        list.printList();
+        list.swapPairs();
+        cout << "\nDLL after swapping pairs:\n";
+        list.printList();
+}
