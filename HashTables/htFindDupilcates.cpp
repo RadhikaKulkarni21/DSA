9586b7373b30295372f87b84b6e831dfa51532ad
radhi <radhika2103@gmail.com>
2025-06-19 16:36:36 +0100

    HT find duplicates

---
+++ HashTables/htFindDupilcates.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+vector<int> findDuplicates(const vector<int>& nums){
+    unordered_map<int, int> numCounts;
+    vector<int> duplicates;
+    //putting each number from vector
+    //Inside the for loop, the count of the current number (num) in the numCounts map is incremented by 1.
+    for(int i : nums){
+        numCounts[i]++;
+    }
+
+    for(const auto& value: numCounts){
+        if(value.second > 1){//seoncd will be value {key,value}
+            duplicates.push_back(value.first);
+        }
+    }
+    return duplicates;
+} 
+
+int main(){
+    auto printVector = [](const vector<int>& v) {
+        cout << "(";
+        for(int i = 0; i < v.size(); i++) {
+            cout << v[i];
+            if (i < v.size() - 1) cout << ", ";
+        }
+        cout << ")";
+    };
+  
+        vector<int> nums = {1, 2, 3, 1, 5, 2};
+
+        vector<int> result = findDuplicates(nums);
+
+        cout << "RETURNED: ";
+        printVector(result);
+}
