5e5fcbd9df389e4e6da852fa83021ea0c36e8828
radhi <radhika2103@gmail.com>
2025-06-25 00:36:45 +0100

    unique characters

---
+++ Sets/stCheckDuplicates.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <unordered_set>
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool hasUniqueChars(const string& str){
+    unordered_set<char> charSet;
+
+    //checking each character from str
+    for(char ch : str){
+        //Check if 'ch' is already in 'charSet' 
+        if(charSet.find(ch)!= charSet.end()){
+            //if its already present in the list, dups are present
+            return false;
+        }
+        charSet.insert(ch);
+    }
+    return true;
+}
+
+int main(){
+    string str = "abcdef";
+    bool output = hasUniqueChars(str);
+    cout<< (output ? "True":"False");
+}
