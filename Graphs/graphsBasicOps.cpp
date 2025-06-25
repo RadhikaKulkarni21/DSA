fcd38de54e079a110465aeb59dffefa08c595672
radhi <radhika2103@gmail.com>
2025-06-25 23:21:41 +0100

    remove vertex and edges

--- Graphs/graphsBasicOps.cpp
+++ Graphs/graphsBasicOps.cpp
@@ -45,15 +45,54 @@ class Graph{
         }
         return false;
     }
+
+    bool removeEdge(string vertex1, string vertex2){
+        if(adjList.count(vertex1)!=0 && adjList.count(vertex2)!=0){
+            adjList.at(vertex1).erase(vertex2);
+            adjList.at(vertex2).erase(vertex1);
+            return true;
+        }
+        return false;
+    }
+
+    /*
+    "A" = ["B","C","D"],
+    "B" = ["A","D"],
+    "C" = ["A","D"],
+    "D" = ["A","B","C"]
+    */
+   bool removeVertex(string vertex){
+    if(adjList.count(vertex) == 0)return false;
+    //this for loop will pick up the "A" in "D" vertex
     
+    for (auto otherVertex : adjList.at(vertex)){
+        //this is gonna return whatever edge is associated with the vertex
+        //then at othervertex "A" it will earse asked vertex which is "D"
+        adjList.at(otherVertex).erase(vertex);
+     }
+     adjList.erase(vertex);
+     return true;
+   }
+   
 };
 
 int main(){
     Graph* newGraph = new Graph();
     newGraph->addVertex("A");
     newGraph->addVertex("B");
+    newGraph->addVertex("C");
+    newGraph->addVertex("D");
 
     newGraph->addEdge("A","B");
-    
+    newGraph->addEdge("A","C");
+    newGraph->addEdge("A","D");
+    newGraph->addEdge("B","D");
+    newGraph->addEdge("C","D");
+    cout<<"Before: "<<"\n";
+    newGraph->printGraph();
+
+    //newGraph->removeEdge("A","B");
+    newGraph->removeVertex("D");
+    cout<<"After: "<<"\n";
     newGraph->printGraph();
 }
