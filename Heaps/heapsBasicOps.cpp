c672cf295453bbd61fea8818d8e4ef8cda91beaa
radhi <radhika2103@gmail.com>
2025-06-26 23:16:00 +0100

    heaps insert

---
+++ Heaps/heapsBasicOps.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <climits> 
+
+using namespace std;
+
+
+class Heap {
+    public:
+    vector<int> heap;
+
+    int leftChild(int index){
+        return 2 * index + 1;
+    }
+
+    int rightChild(int index){
+        return 2 * index + 2;
+    }
+
+    int parent(int index){
+        return (index - 1)/2;
+    }
+
+    void swap(int index1, int index2){
+        int temp = heap[index1];
+        heap[index1] =  heap[index2];
+        heap[index2] = temp;
+    }
+
+    
+    void printHeap() {
+        cout << "\n[";
+        for (size_t i = 0; i < heap.size(); i++) {
+            cout << heap[i];
+            if (i < heap.size() - 1) { 
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+    }
+
+    void insert(int value) {
+        heap.push_back(value);
+        int current = heap.size() - 1;
+
+        //to break out of while => should not be empty and value should always be greater than parent
+        while(current > 0 && heap[current] > heap[parent(current)]){
+            swap(current, parent(current));//swapping to get bigger value 
+            current = parent(current);//change the current pointer for next comparision
+        }
+    }
+};
+
+int main(){
+    Heap* newHeap = new Heap();
+    newHeap->insert(99);
+    newHeap->insert(72);
+    newHeap->insert(61);
+    newHeap->insert(58);
+
+    newHeap->printHeap();  
+    newHeap->insert(100);
+
+     newHeap->printHeap(); 
+}
