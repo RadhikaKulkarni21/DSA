d692055e443d0c3ca12656b79bce79c6dbaff95f
radhi <radhika2103@gmail.com>
2025-06-30 16:05:58 +0100

    minValue from root

--- Recursive/recursionBasicOps.cpp
+++ Recursive/recursionBasicOps.cpp
@@ -82,23 +82,66 @@ public:
     bool rContains(int value){
         return rContains(root, value);
     }
+
+    int minValue(Node* currentNode){
+        while(currentNode->left != nullptr){
+            currentNode = currentNode->left;
+        }
+        return currentNode->value;
+    }
+
+    Node* deleteNode(Node* currentNode, int value){
+        if(currentNode == nullptr) return nullptr;
+        if(value < currentNode->value){
+            currentNode->left = deleteNode(currentNode->left, value);
+        }
+        else{
+            if(currentNode->left == nullptr && currentNode->right == nullptr){
+                delete (currentNode);
+                return nullptr;
+            }
+            else if(currentNode->left == nullptr){
+                Node* temp = currentNode->right;
+                delete(currentNode);
+                return temp;
+            }
+            else if(currentNode->right == nullptr){
+                Node* temp = currentNode->left;
+                delete(currentNode);
+                return temp;
+            }
+            else{
+
+            }
+        }
+        // else if(value > currentNode->value){
+        //     currentNode->right = deleteNode(currentNode->right, value);
+        // }
+        return currentNode;
+    }
+
+    void deleteNode(int value){
+        root = deleteNode(root, value);
+    }
 };
 
 int main(){
     BinarySearchTree* newBst = new BinarySearchTree();
 
-    newBst->rInsert(2);
-    newBst->rInsert(1);
-    newBst->rInsert(3);
-    // newBst->rInsert(18);
-    // newBst->rInsert(27);
-    // newBst->rInsert(52);
-    // newBst->rInsert(58);
+    newBst->rInsert(47);
+    newBst->rInsert(21);
+    newBst->rInsert(76);
+    newBst->rInsert(18);
+    newBst->rInsert(27);
+    newBst->rInsert(52);
+    newBst->rInsert(58);
 
     //cout<<newBst->rContains(27)<<endl;
     //cout<<newBst->rContains(17)<<endl;
-    cout<< "\nRoot:"<<newBst->getRoot()->value;
-    cout<< "\n\nRoot->left:"<<newBst->getRoot()->left->value;
-    cout<< "\n\nRoot->right:"<<newBst->getRoot()->right->value;
+    // cout<< "\nRoot:"<<newBst->getRoot()->value; 
+    // cout<< "\n\nRoot->left:"<<newBst->getRoot()->left->value;
+    // cout<< "\n\nRoot->right:"<<newBst->getRoot()->right->value;
 
+    cout<<newBst->minValue(newBst->root)<<endl;
+    cout<<newBst->minValue(newBst->root->right);
 }
