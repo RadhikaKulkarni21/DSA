f3623e27ce53ab9bcbdeb4b9e008909e67d37a05
radhi <radhika2103@gmail.com>
2025-10-05 16:15:07 +0100

    missing number fixes

--- LeetCode Google/missingNumber.cpp
+++ LeetCode Google/missingNumber.cpp
@@ -18,17 +18,24 @@ int missingNumber(vector<int> arr, int num){
 }
 
 int missingNumberAcc(const vector<int>& arr){
-    int n = arr.size();
+    int n = arr.size() + 1;
     int totalSum = n * (n+1)/2;
 
+    //to replace accumulate
+    int actualSum = 0;
+    for(int num : arr){
+        actualSum += num;
+    }
+
+    //accumulate only works if 0 is included
     return totalSum - accumulate(arr.begin(),arr.end(),0);
 }
 
 int main(){
     vector<int> arr = {1,2,3,4,5,6,8,9,10};
-    vector<int> num = {1,2,3,4,7,6,8,9,10};
+    vector<int> num = {4,6,7,1,9,8,3,5,10};
     int n = arr.size() + 1;//need a +1 bc array index starts from zero
-    cout<< "Missing Number: "<< missingNumber(arr, n)<< endl;
+    //cout<< "Missing Number: "<< missingNumber(arr, n)<< endl;
     cout<< "Missing Number: "<< missingNumberAcc(num);
     return 0;
 }
