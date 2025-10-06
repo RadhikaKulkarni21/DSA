0db588926ef3f6d93c6aee12ab3977e082d5f181
radhi <radhika2103@gmail.com>
2025-10-06 20:42:40 +0100

    base

---
+++ OOPS/baseDerivedClass.cpp
@@ -0,0 +1,57 @@
+#include "oopsHeader.h"
+
+class Animal{
+    public:
+    void eat(){
+        cout<<"Eating";
+    }
+};
+
+class Dog : public Animal{
+    public:
+    void barks(){
+        cout<<"Woof";
+    }
+};
+
+class Cat : public Animal{
+    public:
+    void meows(){
+        cout<<"Meow";
+    }
+};
+
+//Using virtual keyword for overriding
+class VAnimal {
+public:
+    virtual void eat() {  // virtual = can be overridden
+        cout << "Animal eats" << endl;
+    }
+};
+
+class Horse : public VAnimal {
+public:
+    void eat() override {
+        cout << "Horse eats" << endl;
+    }
+};
+
+int main(){
+    Animal a;
+    a.eat();//works
+    //a.bark(); does not work bc derived class
+
+    Dog d;
+    d.eat();//derived
+    d.barks();//own
+
+    Animal* c = new Dog();
+    c->eat();
+    //c->barks(); won't work as you cant reference to dervied function
+
+    //Using virtual method
+    VAnimal *h = new Horse();
+    h->eat();   // Calls Dog's version because of virtual
+    delete h;
+    return 0;
+}
