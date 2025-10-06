6ac6b896b8aa3a65ba207c1b673df31065e00fbb
radhi <radhika2103@gmail.com>
2025-10-06 20:54:20 +0100

    Constructors

---
+++ OOPS/constructors.cpp
@@ -0,0 +1,48 @@
+#include "oopsHeader.h"
+
+//simple constructor
+class A {
+public:
+    A() { cout << "Default constructor called" << endl; }
+};
+
+//Parameterized constructor
+class Student {
+public:
+    string name;
+    int age;
+
+    Student(string n, int a) {   // Parameterized constructor
+        name = n;
+        age = a;
+    }
+};
+
+//copy constructor
+class Student {
+public:
+    string name;
+    int age;
+
+    Student(string n, int a) {
+        name = n;
+        age = a;
+    }
+
+    // Copy constructor
+    Student(const Student &s) {
+        name = s.name;
+        age = s.age;
+    }
+};
+
+int main() {
+    //parameterized constructor
+    Student s1("Alice", 20);
+    cout << s1.name << " " << s1.age << endl;
+
+    //Copy constructor
+    Student s1("Bob", 22);
+    Student s2 = s1;  // Copy constructor called
+    cout << s2.name << " " << s2.age << endl;
+}
