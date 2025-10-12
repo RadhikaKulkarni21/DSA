cdbe92c4513a65ac93d19602920a964c166ba412
radhi <radhika2103@gmail.com>
2025-10-12 17:05:20 +0100

    switch case

---
+++ OOPS/switchCase.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int switchCase(int n){
+    switch(n){
+        case 1:
+        cout<<"day 1"<<endl;
+        break;
+
+        case 2:
+        cout<<"day 2"<<endl;
+        break;
+
+        default:
+        cout<<"No day"<<endl;
+        break;
+    }
+    return 0;
+}
+
+int main(){
+    int n = 2;
+    switchCase(2);
+    //switchCase(3);
+}
