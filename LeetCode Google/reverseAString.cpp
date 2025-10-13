59ebb6cf11c402a69ccc53075b952edcdacec3e6
radhi <radhika2103@gmail.com>
2025-10-13 16:01:35 +0100

    basic

--- LeetCode Google/reverseAString.cpp
+++ LeetCode Google/reverseAString.cpp
@@ -18,6 +18,20 @@ Input: s = "the sky is blue"
 Output: "blue is sky the"
 */
 
+//basic reverse string
+void reverseString(vector<char>& s) {
+    int i  = 0;
+    int j = s.size() - 1;
+
+    while(i < j){
+        char temp = s[i];
+        s[i] =  s[j];
+        s[j] = temp;
+        i++;
+        j--; 
+    }
+}
+
 string reverseWords(string s){
     string reverse;
     string word;
