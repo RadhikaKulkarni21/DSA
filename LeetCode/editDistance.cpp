5843b42c97f813f7b8588b8a4069885e7798723d
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-01-03 20:38:15 +0000

    expl

--- LeetCode/editDistance.cpp
+++ LeetCode/editDistance.cpp
@@ -41,9 +41,9 @@ class Solution{
         if(cache[i][j] != -1) return cache[i][j];
 
         if(word1[i - 1] == word2[j - 1]){
-            cache[i][j] = dp(word1, word2, i - 1, j - 1, cache);
+            cache[i][j] = dp(word1, word2, i - 1, j - 1, cache);//words match, we move next
         } else {
-            cache[i][j] = 1 + min({
+            cache[i][j] = 1 + min({//letters dont match we do this
                 dp(word1, word2, i - 1, j, cache),     // Delete
                 dp(word1, word2, i, j - 1, cache),     // Insert
                 dp(word1, word2, i - 1, j - 1, cache)  // Replace
@@ -52,3 +52,45 @@ class Solution{
         return cache[i][j];
     }
 };
+
+int main(){
+    string word1 = "horse";
+    string word2 = "ore";
+
+    Solution s;
+    cout << "Min changes: " << s.minDistance(word1,word2);
+}
+
+/*
+i will be the amount of letters in word 1 
+j will be the amount of letters in word 2
+   i j
+dp(3,3)	"cat" → "cut"
+dp(2,2)	"ca" → "cu"
+dp(1,1)	"c" → "c"
+dp(0,2)	"" → "cu"
+
+we take the minimum cost required to fix the word
+here the replacing of a with u takes min cost
+
+we go matching each of the last character and when we hit a mismatch we 
+either insert, delete or replace the character
+if we delete it becomes i - 1, j
+if we insert it becomes i, j - 1
+replace will be i - 1, j - 1
+
+
+cat      cut
+t is same for both so
+ca  cu
+
+now we have three operations
+delete   insert   replace
+c => cu  ca => c  c => c
+
+we need to think out of all the tree which would require the least
+cost next time the problem runs
+hence here it would be replace
+
+Check image folder for more detailed long explanation
+*/
