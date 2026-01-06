94385a579bacdeb44ab29d2d12097a1175d75e2a
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-01-06 13:31:45 +0000

    ex

--- LeetCode/NumberOf1Bit.cpp
+++ LeetCode/NumberOf1Bit.cpp
@@ -13,9 +13,12 @@ Explanation:
 
 The input binary string 1011 has a total of three set bits.
 */
+
 #include <bitset>
 #include "R:\C++\Project1\lcHeader.h"
 
+//this loop will run exactly 32 times as the conversio is 32
+//so the time complexity stays O(32) == O(1)
 int hammingWeight(int n) {
     bitset<32> dec(n);
     int counter = 0;
@@ -25,6 +28,17 @@ int hammingWeight(int n) {
         }
     }
     return counter;
+}
+
+//idk why we are using gates when the time complexity and space complexity remains the same
+int hammingWGate(uint32_t n){
+    int res = 0;
+    for (int i = 0; i < 32; i++) {
+        if ((n >> i) & 1) {
+            res += 1;
+        }
+    }
+    return res;        
 }
 
 int main(){
