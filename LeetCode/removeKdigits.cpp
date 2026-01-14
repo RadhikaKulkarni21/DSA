3404bdb97404f6b17e195b6a2edda524a4ee0569
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-01-14 23:23:03 +0000

    kd

--- LeetCode/removeKdigits.cpp
+++ LeetCode/removeKdigits.cpp
@@ -1,1 +1,32 @@
+/*
+Given string num representing a non-negative integer num,
+and an integer k, return the smallest possible integer after removing k digits from num.
+*/
+
 #include "R:\C++\Project1\lcHeader.h"
+
+string removeKdigits(string num, int k)
+{
+    string ans = "";
+    for (char &c : num){
+        while (ans.size() && ans.back() > c && k)
+        {
+            ans.pop_back();
+            k--;
+        }
+        if (ans.size() || c != '0'){
+            ans.push_back(c);
+        }  
+    }
+    while (ans.size() && k--){
+        ans.pop_back();
+    }
+    return (ans == "") ? "0" : ans;
+}
+
+int main(){
+    string num = "10200";
+    int k = 1;
+
+    cout<<removeKdigits(num, k);
+}
