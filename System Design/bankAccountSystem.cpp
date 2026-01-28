2fe882c9d2f758074c10a8dd28c572f50c793f34
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-01-28 15:52:37 +0000

    bas

--- System Design/bankAccountSystem.cpp
+++ System Design/bankAccountSystem.cpp
@@ -5,23 +5,22 @@
 //transactions
 
 enum TransactionType{
-    withdraw,
-    depsoit
+    WITHDRAW,
+    DEPOSIT
 };
+
 //we can enum account types as well
 //But we are using inheritence so can skip
-
 class Account{
 protected:
     int accId;
     double balance;
     
 public:
-    Account(int i, double b) : accId(i), balance(b){};
-
     //withdraw
     //deposit
     //getbalance
+    Account(int i, double b) : accId(i), balance(b){};
 
     void deposit(double amount){
         balance += amount;
@@ -29,15 +28,18 @@ public:
 
     //we are putting drafting limit so will be different for different accounts
     virtual bool withdraw(double amount) = 0;
-    // void withdraw(double amount){
-    //     balance -= amount;
-    // }
 
     virtual string getAccountType() = 0;
+
     double getBalance(){
         return balance;
     }
 
+    int getAccountId(){
+        return accId;
+    }
+
+    virtual ~Account(){};
 };
 
 class Savings : public Account{
@@ -100,7 +102,99 @@ public:
     Transactions(int i, TransactionType t, double a, int an) : tId(i), type(t), amount(a), accNo(an){};
 };
 
+class Bank{
+public:
+    vector<Customer> customers;
+    vector<Transactions> transactions;
+
+    //This is to give transaction an ID
+    int transactionIdCounter = 1;
+
+
+    void addCustomer(Customer c){
+        customers.push_back(c);
+    }
+
+    //We need to reference the customers here
+    void addAccount(int cId, Account* acc){
+        for(auto& c : customers){
+            if(c.cusId == cId){
+                c.accounts.push_back(acc);
+                return;
+            }
+        }
+    }
+
+    bool depositMoney(int accId, double amount){
+        for(auto& c : customers){
+            for(auto a : c.accounts){
+                if(a->getAccountId() == accId){
+                    a->deposit(amount);
+                    transactions.push_back(Transactions(transactionIdCounter++, DEPOSIT, amount, accId));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool withDrawMoney(int accId, double amount){
+        for(auto c : customers){
+            for(auto a : c.accounts){
+                if(a->getAccountId() ==  accId){
+                    if(a->withdraw(amount)){
+                        transactions.push_back(Transactions(transactionIdCounter++, WITHDRAW, amount, accId));
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    void printTransactions(){
+        for(auto t : transactions){
+            cout << "Transaction ID: " << t.tId
+            << " Account: " << t.accNo
+            << " Type: " << (t.type == DEPOSIT ? "Deposit" : "Withdraw")
+            << " Amount: " << t.amount << endl;
+        }
+    }
+
+};
+
 
 int main(){
+    Bank bank;
 
+    Customer c1(1, "Kanavi");
+    bank.addCustomer(c1);
+
+    Account* savings = new Savings(101, 1000, 500);
+    Account* current = new Current(102, 500, 1000);
+
+    bank.addAccount(1, savings);
+    bank.addAccount(1, current);
+
+    bank.depositMoney(101, 300);
+    bank.depositMoney(102, 500);
+
+    bank.withDrawMoney(101, 500);
+    bank.withDrawMoney(102, 1200);
+
+
+    cout << "Balance:" << endl;
+    cout << "Savings: " << savings->getBalance() << endl;
+    cout << "Current: " << current->getBalance() << endl;
+    
+    if(bank.transactions.size() > 0){
+        cout << "Account Transactions" << endl;
+        bank.printTransactions();
+    }
+    else{
+        cout << "No transactions to display";
+    }
+
+    delete savings;
+    delete current;
 }
