e2139e2b735e42d08720175191051fe95320149e
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-02-03 22:44:24 +0000

    update

--- LibraryManagementSystem.cpp
+++ LibraryManagementSystem.cpp
@@ -6,17 +6,62 @@ Library Management System
 */
 
 class Book{
+public:
+    string title;
+    string author;
+    int id;
+    string category;
+
+
+    Book(string t, string a, int i, string cat) : title(t), author(a), id(i), category(cat) {};
+
+    void displayBook(){
+        cout<< "Book: " << title << " by " << author << endl;
+    }
 
 };
 
 class Member{
+public:
+    int memberId;
+    string mName;
 
+    Member(int mid, string n) : memberId(mid), mName(n) {};
+
+    void displayMember(){
+        cout << "Member Id: " << memberId << "Member Name: " << endl;
+    }
 };
 
-class Librabrian{
+class Librarian{
+public:
+    int LibId;
+    string lName;
 
+    Librarian(int lid, string ln) : LibId(lid), lName(ln) {};
 };
 
-class Transaction{
+class LateFine{
+public: 
+    int returnDate;
+    int actualReturnDate;//for late fees
+    double lateFees;
+
+    LateFine(int rd, int ard) : returnDate(rd), actualReturnDate(ard) {};
+
+    int totalLateDates = actualReturnDate - returnDate;
+
+    double calculateFine(int lateDays){
+        return lateDays * lateFees;
+    }
+};
+
+class Transactions{
+public:
+    int issueDate;
     
 };
+
+int main(){
+
+}
