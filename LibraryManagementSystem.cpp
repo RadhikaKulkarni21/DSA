4cc2132b29bf5e5086e0aad376ccdbe211c83943
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-02-03 20:58:00 +0000

    lm

---
+++ LibraryManagementSystem.cpp
@@ -0,0 +1,22 @@
+#include "R:\C++\Project1\lcHeader.h"
+/*
+Library Management System
+ o Classes: Book, Member, Librarian, Transaction.
+ o Add-ons: issue/return logic, due date fine
+*/
+
+class Book{
+
+};
+
+class Member{
+
+};
+
+class Librabrian{
+
+};
+
+class Transaction{
+    
+};
