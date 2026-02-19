41d13e759d3f11924c9a3ac9ea5b25d37268a238
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-02-19 19:54:01 +0000

    elevator

--- System Design/elevatorSystem.cpp
+++ System Design/elevatorSystem.cpp
@@ -13,13 +13,85 @@ Elevators should not exceed capacity.
 
 #include "R:\C++\Project1\lcHeader.h"
 
+enum Direction{
+    UP,
+    DOWN,
+    IDLE
+};
+
 class Elevator{
 public:
     int elevatorId;
     double capacity;
-    float doorDuration;
+    int currentFloor;
+    int currentLoad;
+    Direction direction;
 
-    Elevator(int ei, double c, float dr) : elevatorId(ei), capacity(c), doorDuration(dr) {};
+    vector<int> requests;
+
+    Elevator(int ei, double c, int cf, int cl, Direction(d)) : elevatorId(ei), capacity(c), currentFloor(cf), currentLoad(cl), direction(d) {};
+
+    int getCurrentFloor(){
+        return currentFloor;
+    }
+
+    Direction getDirection(){
+        return direction;
+    }
+
+    bool isIdle() const {
+        return requests.empty();
+    }
+
+    void addRequest(int floor){
+        requests.push_back(floor);
+        updateDirection();
+        sortRequests();
+    }
+
+    void updateDirection(){
+        if(requests.empty()){
+            direction = IDLE;
+            return;
+        }
+        if (currentFloor < requests.front())
+            direction = UP;
+        else if (currentFloor > requests.front())
+            direction = DOWN;
+    }
+
+    void sortRequests(){
+        if (direction == UP)
+            sort(requests.begin(), requests.end());
+        else if (direction == DOWN)
+            sort(requests.rbegin(), requests.rend());
+    }
+
+    void openDoor(){
+        cout << "Door Opening at " << "Floor: " << currentFloor << endl;
+    }
+    void move(){
+        if(requests.empty()){
+            direction = IDLE;
+            return;
+        }
+
+        int goToFloor = requests.front();
+
+        if(currentFloor > goToFloor){
+            currentFloor--;
+            direction = DOWN;
+        }
+        else if(currentFloor < goToFloor){
+            currentFloor++;
+            direction = UP;
+        }
+
+        if(currentFloor == goToFloor){
+            openDoor();
+            requests.erase(requests.begin());
+        }
+    }
 };
 
 class Building{
@@ -29,12 +101,6 @@ public:
     int totalAvailableElevators;
 
     Building(int f, int tf, int tae) : floor(f), totalFloors(tf), totalAvailableElevators(tae){};
-};
-
-class Direction{
-public:
-    bool goingup;
-    bool goingDown;
 };
 
 class Operation{
