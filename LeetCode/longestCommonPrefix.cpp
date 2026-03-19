28f613e37bde73a39ff181ce8db62489bbecd8dd
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-03-19 22:50:39 +0000

    ex

--- LeetCode/longestCommonPrefix.cpp
+++ LeetCode/longestCommonPrefix.cpp
@@ -15,18 +15,22 @@ class Solution{
 public:
     string longestCommonPrefix(vector<string>& strs){
         if(strs.empty()) return "";
-
+        
+        //considering the first word in string as the prefix
         string pref = strs[0];
         int n = pref.length();
 
         for(int i = 1;i < strs.size();i++){
             string s = strs[i];
-
+            //compare each character in prefix with next string
+            //if the string does not match we remove one character from the prefix
+            //keep removing until the string is empty or we get a matching character
             while(n > s.length() || pref != s.substr(0,n)){
                 n--;
                 if(n == 0){
                     return "";
                 }
+                //update with whatever the same characters is
                 pref = pref.substr(0,n);
             }
         }
