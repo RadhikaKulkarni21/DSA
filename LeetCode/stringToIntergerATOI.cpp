23e559ab7f8ac9dfb9b73fe0bf915c1bfea93f96
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-03-31 19:39:37 +0100

    correct code

--- LeetCode/stringToIntergerATOI.cpp
+++ LeetCode/stringToIntergerATOI.cpp
@@ -9,7 +9,10 @@ assuming positivity if neither present.
 Conversion: Read the integer by skipping leading zeros until a 
 non-digit character is encountered
 or the end of the string is reached. If no digits were read, then the result is 0.
-
+Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1],
+then round the integer to remain in the range. 
+Specifically, integers less than -231 should be rounded to -231,
+and integers greater than 231 - 1 should be rounded to 231 - 1.
 Example 1:
 
 Input: s = "42"
@@ -57,13 +60,52 @@ public:
 
 //correct solution
 class Solution2{
+public:
     int myAtoi(string s){
+        if(s.size() == 0){
+            return 0;
+        }
+
+        const long long maxInt = INT_MAX;
+        const long long minInt = INT_MIN;
+
+        int sign = 1;
+        long long res = 0;
         
+        //whitespace
+        int i = 0;
+        while(i  < s.size() && s[i] == ' '){
+            i++;
+        }
+
+        //sign -/+
+        if(s[i] == '+'){
+            i++;
+        }
+        else if(s[i] == '-'){
+            sign = -1;
+            i++;
+        }
+
+        //digits only
+        while(i < s.size() && isdigit(s[i])){
+            int digit = s[i] - '0';
+            res = res * 10 + digit;
+
+            if(sign * res >= maxInt) return maxInt;
+            if(sign * res <= minInt) return minInt;
+
+            i++;
+        }
+
+        return static_cast<int>(res * sign);
     }
 };
 
 int main(){
     Solution sol;
+    Solution2 sol2;
     string s = "-42ppo";
-    cout<<sol.myAtoi(s);
+    //cout<<sol.myAtoi(s);
+    cout<<sol2.myAtoi(s);
 }
