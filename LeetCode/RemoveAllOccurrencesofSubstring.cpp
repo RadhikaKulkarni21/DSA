7c4b9c481d1ce4ba7e9917e9e0b53f3113069217
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-04-02 22:20:17 +0100

    comments

--- LeetCode/RemoveAllOccurrencesofSubstring.cpp
+++ LeetCode/RemoveAllOccurrencesofSubstring.cpp
@@ -13,10 +13,22 @@ A substring is a contiguous sequence of characters in a string.
 class Solution{
 public:
     string removeOccurrences(string s, string part){
-        
+        //find where the first occurance of part occurs
+        size_t index = s.find(part);
+        //repeatedly remove till we reach the end of the string
+        while(index != string::npos){
+            //modify the string
+            s.erase(index, part.length());
+            //update index each time we remove it
+            index = s.find(part);
+        }
+        return s;
     }
 };
 
 int main(){
     Solution sol;
+    string s = "axxxxyyyyb";
+    string part = "xy";
+    cout << sol.removeOccurrences(s, part);//expected: ab
 }
