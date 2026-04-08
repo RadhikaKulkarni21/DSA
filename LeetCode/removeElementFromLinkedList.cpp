7fb444400407622575b44f6b4e114bbc539da087
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-04-08 14:54:46 +0100

    explanation

--- LeetCode/removeElementFromLinkedList.cpp
+++ LeetCode/removeElementFromLinkedList.cpp
@@ -3,15 +3,23 @@
 class Solution{
 public:
     ListNode* removeElements(ListNode* head, int val){
+        //create a new node with value 0 and head as next node
         ListNode* ans = new ListNode(0, head);
+        //set the ans to dummy
         ListNode* dummy = ans;
         
-        while(dummy != nullptr){
+        while(dummy != nullptr){//iterate till there is no next node
+            //as long as next exist and value of next exist we skip the 
+            //current node and move to next next
+            //the outer dummy moves once  
+            //inner dummy checks the next to skip or not
             while(dummy->next != nullptr && dummy->next->val == val){
                 dummy->next = dummy->next->next;
             }
+            //update to move forward
             dummy = dummy->next;
         }
+        //we need next as ans is 0/dummy
         ListNode* result = ans->next;
         delete ans;
 
@@ -20,6 +28,13 @@ public:
 };
 
 int main(){
-    ListNode* head = new ListNode(20);
-    head->next = new ListNode(30);
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(6);
+    head->next->next->next = new ListNode(3);
+    int val = 6;
+
+    Solution sol;
+    sol.removeElements(head, val);
+    cout<< head->val;
 }
