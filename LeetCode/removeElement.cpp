0c0abe90178668050b8ab84b2fb70a4d6ca56e4d
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2026-04-08 14:15:16 +0100

    comments

--- LeetCode/removeElement.cpp
+++ LeetCode/removeElement.cpp
@@ -8,9 +8,13 @@ class Solution{
 public:
     int removeElement(vector<int>& nums, int val){
         int k = 0;
+        //we iterate through all the the nums
         for(int i= 0; i < nums.size(); i++){
+            //just check if the val is not equal 
+            //move the elements with non-val to the front
             if(nums[i] != val){
                 nums[k] = nums[i];
+                //each time the replacement occurs just increase the k
                 k++;
             }
         }
@@ -19,7 +23,7 @@ public:
 };
 
 int main(){
-    vector<int> nums = {2,3,3,2};
+    vector<int> nums = {2,3,3,2};//exp op: 2
     int val = 2;
     Solution sol;
     cout<< sol.removeElement(nums, val);
